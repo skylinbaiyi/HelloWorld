b42a3c1ccd499462f3ba9d5ca2bbb870a77582eb
skylinbaiyi <skylin_baiyi@126.com>
2015-12-30 16:25:57 +0800

    初始化:修改md文件,添加初始的工程

---
+++ HelloWorld/HelloWorld.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorld.h
+//  HelloWorld
+//
+//  Created by Netease on 15/12/29.
+//  Copyright © 2015年 Netease. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HelloWorld : NSObject
+
++ (NSString *)hello;
+
+@end
---
+++ HelloWorld/HelloWorld.m
@@ -0,0 +1,18 @@
+//
+//  HelloWorld.m
+//  HelloWorld
+//
+//  Created by Netease on 15/12/29.
+//  Copyright © 2015年 Netease. All rights reserved.
+//
+
+#import "HelloWorld.h"
+
+@implementation HelloWorld
+
++ (NSString *)hello
+{
+    return @"Hello";
+}
+
+@end
